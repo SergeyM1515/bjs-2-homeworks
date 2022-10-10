e230bd46e2932fd1dca52228719d1b1944b7a3bc
Sergey <Mkrtychantssergey@gmail.com>
2022-10-10 22:05:45 +0300

    initial

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,24 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  if(a == 0)
+        return false;
+  let arr = [];
+  let d = b**2 - 4*a*c;
+  let x1, x2;
+  if (d < 0) {
+    d = arr[''];
+  }
+  if (d == 0) {
+    x1 = -b / (2 * a);
+    return [x1];
+  }
+  else if (d > 0) {
+  x1 = ((-b + Math.sqrt(d)) / (2 * a));
+  x2 = ((-b - Math.sqrt(d)) / (2 * a));
+  return [x1,x2];
+  }
+  return arr; 
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
