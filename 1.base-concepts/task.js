4c6eed8eb35890cb7f30ddcf3aaa495ad4cf5ad1
Sergey <Mkrtychantssergey@gmail.com>
2022-10-15 19:20:09 +0300

    rework

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,25 +1,21 @@
 "use strict";
 
 function solveEquation(a, b, c) {
-  if(a == 0)
-        return false;
+  if(a == 0) {return false};
   let arr = [];
   let d = b**2 - 4*a*c;
   let x1, x2;
-  if (d < 0) {
-    d = arr[''];
-  }
   if (d == 0) {
     x1 = -b / (2 * a);
     return [x1];
   }
   else if (d > 0) {
-  x1 = ((-b + Math.sqrt(d)) / (2 * a));
-  x2 = ((-b - Math.sqrt(d)) / (2 * a));
-  return [x1,x2];
+    x1 = ((-b + Math.sqrt(d)) / (2 * a));
+    x2 = ((-b - Math.sqrt(d)) / (2 * a));
+    return [x1,x2];
   }
-  return arr; 
-}
+    return arr; 
+  }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
